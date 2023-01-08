4887efee93984f8401d464fa9ee88cdc9fdb25ae
Baka <baka@iMac-AFD.local>
2023-01-08 22:02:32 +0300

    add product list

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -22,7 +22,7 @@ const Form = (props) => {
         return () => {
             tg.offEvent('mainButtonClicked', onSendData)
         }
-    }, [])
+    }, [onSendData])
 
     useEffect(() => {
         tg.MainButton.setParams({
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import ProductItem from "../ProductItem/ProductItem";
 import './ProductList.css'
 
+const products = [
+    {id: 1, title: 'Геральт из Ривии', price: 10000, description: 'Ватная игрушка Геральт из Ривии'},
+    {id: 2, title: 'Джофри Баратеон', price: 1000, description: 'Ватная игрушка Джофри'},
+    {id: 3, title: 'Валера', price: 19000, description: 'Ватная игрушка Валера, время которого настало'},
+    {id: 4, title: 'Зигмунд', price: 90000, description: 'Ватная игрушка Зигмунд Усы Таракана'},
+]
+
 const ProductList = (props) => {
+    const [addedItems, setAddedItems] = useState([])
+
+    const onAdd = (product) => {
+        
+    }
+
     return (
-        <div>
-            PRODUCTLIST
+        <div className={'list'}>
+            {products.map(item => 
+                <ProductItem 
+                product={item}
+                className={'item'}
+                onAdd={onAdd}
+                />
+                )}
         </div>
     )
 }
